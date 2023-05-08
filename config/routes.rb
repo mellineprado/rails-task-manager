9224adb00399c0277f0d4df1810f5f59bcb1f7fa
melline prado <mellinevprado@hotmail.com>
2023-05-08 16:33:58 -0300

    created controller, actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # # list all tasks
+  # get 'tasks', to: 'tasks#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # # show a form to create a new task
+  # get 'tasks/new', to: 'tasks#new'
+
+  # # show a task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # save task into db
+  # post 'tasks', to: 'tasks#create'
+
+  # # show a form to update a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+
+  # # update task info into db
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # delete a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
